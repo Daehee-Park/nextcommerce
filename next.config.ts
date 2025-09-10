d91a0186ed2c74aedcbaee2b66deabf13106d497
Daehee-Park <400yad@gmail.com>
2025-09-10 20:48:30 +0900

    next.config 이미지 최적화 설정 1. 이미지 포맷 및 크기 최적화 2. 도메인 화이트리스트 설정 (SSRF/cache 남용 방지)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp'], // 이미지 포맷 최적화
+    remotePatterns: [ // SSRF(Server-Side Request Forgery 방지, 캐시 남용 방지)
+      { protocol: 'https', hostname: 'picsum.photos' }
+    ],
+    deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920], // 반응형 이미지 크기 최적화 (화면 크기에 따라 이미지 크기 조절)
+  },
 };
 
 export default nextConfig;
